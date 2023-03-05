4ff1f80cc0418843be3f2c465d7ba8b47cdb7aea
SaravananJaikumar <127012040+SaravananJaikumar@users.noreply.github.com>
2023-03-05 13:48:27 +0530

    Add files via upload
    Day 1 assignments

---
+++ IsPrime.java
@@ -0,0 +1,16 @@
+package day1.assignment;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		int num=57;
+		boolean isprime=true;
+		for (int i = 2; i < num; i++) {
+			if (num%i==0);
+				
+			break;
+		}
+System.out.println(isprime);
+	}
+
+}
